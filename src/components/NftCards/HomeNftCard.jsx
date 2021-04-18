9e3623560c7aeb5db0d6b7673414171e66ec1c55
pnkfluffy <jacksonfelty@gmail.com>
2021-04-17 20:56:52 -0600

    progress on nft cards

--- src/components/NftCards/HomeNftCard.jsx
+++ src/components/NftCards/HomeNftCard.jsx
@@ -1,10 +1,11 @@
 import React, { useCallback, useEffect, useState } from "react";
 import styled from "styled-components";
 import image from "../../assets/img/logos/fm_logo_1.png";
-import cart from "../../assets/img/listen/cart.svg";
 import Modal from "../Modal/Modal";
 import BuyNftModal from "../BuyNftModal";
 import { ReactComponent as IconHeart } from "../../assets/img/Icons/heart.svg";
+import { ReactComponent as IconShare } from "../../assets/img/Icons/share.svg";
+import {ReactComponent as IconCart} from "../../assets/img/Icons/cart.svg";
 
 const NftCard = (props) => {
   const { nft } = props;
@@ -15,21 +16,37 @@ const NftCard = (props) => {
     console.log("isOpen", isOpen);
   };
   console.log('nft', nft)
+
+  const like = () => {
+    //${!}
+  }
+
+  const share = () => {
+    //${!}
+  }
+
   return (
     <Container>
       <BuyNftModal open={isOpen} hide={hide} nft={nft} />
       <CardTop>
         <Side>
           <IconArea>
-            <Heart />
-            {nft?.likeCount}
+            <Heart onClick={() => like()} />
+            {nft.likeCount}
           </IconArea>
           <IconArea>
-            {nft?.shareCount}
+            <Share onClick={() => share()} />
+            {nft.shareCount}
           </IconArea>
         </Side>
         <Side>
-
+          <IconArea>
+            {nft.x_numSold}<span style={{margin: "0 1px"}}>
+              /
+              </span>
+              {nft.numMinted}
+            <Cart/>
+          </IconArea>
         </Side>
       </CardTop>
       <Image src={nft.imageUrl} alt="image" onClick={() => setIsOpen(!isOpen)} />
@@ -38,29 +55,60 @@ const NftCard = (props) => {
           <TrackName>{nft.title}</TrackName>
           <Artist>{nft.artist}</Artist>
         </InfoContainer>
-        <Buy src={cart} alt="cart" onClick={() => setIsOpen(!isOpen)} />
       </BottomContainer>
     </Container>
   );
 };
 
-const Heart = styled(IconHeart)`
-width: 18px;
-height: 18px;
-/* fill: pink; */
+const Cart = styled(IconCart)`
+width: 20px;
+height: 20px;
+margin: -2px 0 0 8px;
 cursor: pointer;
-transition: all 0.2s linear;
-/* margin-top: -30px; */
+transition: all 0.2s ease-in-out;
  & path {
+    transition: all 0.2s ease-in-out;
+     fill: ${props => props.theme.color.gray};
+    }
+&:hover {
+  & path {
+    fill: #20a4fc;
+  }
+}
+`
+
+const Share = styled(IconShare)`
+width: 16px;
+height: 16px;
+margin: 0 4px 0 0;
+cursor: pointer;
+transition: all 0.2s ease-in-out;
+ & path {
+    transition: all 0.2s ease-in-out;
+     fill: ${props => props.theme.color.gray};
+    }
+&:hover {
+  & path {
+    fill: #20a4fc;
+  }
+}
+`
+
+const Heart = styled(IconHeart)`
+width: 20px;
+height: 20px;
+margin: -3px 4px 0 0;
+cursor: pointer;
+transition: all 0.2s ease-in-out;
+ & path {
+    transition: all 0.2s ease-in-out;
      stroke: ${props => props.theme.color.gray};
     }
-
-    &:hover {
-      & path {
-     stroke: pink;
-    }
+&:hover {
+  & path {
+    stroke: #DD4591;
+  }
 }
-
 `
 
 // const Heart = styled(SVG)`
@@ -86,20 +134,25 @@ align-items: center;
 const IconArea = styled.div`
   margin: 0 8px;
   display: flex;
-  font-size: ${props => props.theme.fontSizes.xs};
+  font-size: 14px;
+  height: 100%;
+  align-items: center;
 `
 
 const CardTop = styled.div`
-width: calc(100% - 24px);
-padding: 12px;
+/* width: calc(100% - 4px); */
+/* padding: 0px 2px; */
+width: 100%;
+margin-bottom: 8px;
 display: flex;
 justify-content: space-between;
-color: ${props => props.theme.color.gray};
 font-weight: bold;
 font-family: Compita;
 `
 
 const Container = styled.div`
+  color: ${props => props.theme.color.gray};
+  padding: 12px;
   background-color: ${props => props.theme.boxColor};
   border: 1px solid ${props => props.theme.boxBorderColor};
   border-radius: ${props => props.theme.borderRadius}px;
@@ -113,14 +166,15 @@ const Image = styled.img`
   cursor: pointer;
   width: 200px;
   height: 200px;
-  border-radius: 15px;
+  border-radius: 12px;
   object-fit: cover;
+  margin-bottom: 12px;
 `;
 
 const BottomContainer = styled.div`
-  width: 200px;
+  width: 100%;
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
   align-items: center;
 `;
 
@@ -133,7 +187,11 @@ const InfoContainer = styled.div`
 `;
 
 const TrackName = styled.span`
-  font-size: 0.9rem;
+color: white;
+font-weight: bold;
+width: 100%;
+text-align: center;
+font-size: ${props => props.theme.fontSizes.xs}px;
 `;
 
 const Artist = styled.span`

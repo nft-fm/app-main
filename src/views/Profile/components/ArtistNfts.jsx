e3d96c60ed650bb924d172e2bbe32ef9b6b89a4c
Quinn <whyquinn@gmail.com>
2021-04-19 10:55:50 -0500

    .

---
+++ src/views/Profile/components/ArtistNfts.jsx
@@ -0,0 +1,114 @@
+import React, { useCallback, useEffect, useState } from "react";
+import styled, { keyframes } from "styled-components";
+import axios from "axios";
+import NftCard from "../../components/NftCards/SaleNftCard";
+
+
+const ArtistNfts = () => {
+    const [nfts, setNfts] = useState([])
+  
+    useEffect(() => {
+    //   axios.get("/api/nft-type/featured").then((res) => setNfts(res.data));
+    }, [])
+  
+    const showNfts = nfts.map((nft) => {
+      return (
+        <NftCard nft={nft} />
+      )
+    }); 
+  
+    return (
+      <Landing>
+        <LaunchContainer>
+          <ContainerTitle>
+            PROUD TO
+            <ContainerTitleText style={{ "color": "#20a4fc" }}>{`LAUNCH`}</ContainerTitleText>
+            <ContainerTitleText style={{ "color": "#fde404" }}>{`ALONGSIDE`}</ContainerTitleText>
+            <ContainerTitleText style={{ "color": "#68c12f" }}>{`AMAZING`}</ContainerTitleText>
+            <ContainerTitleText style={{ "color": "#fa423e" }}>{`ARTISTS`}</ContainerTitleText>
+          </ContainerTitle>
+          <ContainerOutline />
+          <NftScroll> {showNfts} </NftScroll>
+        </LaunchContainer>
+      </Landing >
+    );
+  };
+  
+  
+  
+  const ContainerTitleText = styled.span`
+  padding-left: 6px;
+  `
+  
+  const Landing = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  /* height: calc(100vh - ${props => props.theme.topBarSize}px + 1px); */
+  width: ${props => props.theme.homeWidth}px;
+  max-width: 80vw;
+  padding-top: 40px;
+  color: white;
+    font-size: ${props => props.theme.fontSizes.xs};
+    padding-right: 4px;
+  `
+  
+  const NftScroll = styled.div`
+  justify-content: center;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-between;
+  @media only screen and (max-width: 1500px) {
+    & > * {
+      &:nth-last-child(1) {
+          display: none;
+     }}}
+     @media only screen and (max-width: 1191px) {
+    & > * {
+      &:nth-last-child(2) {
+          display: none;
+     }}}
+     @media only screen and (max-width: 890px) {
+    & > * {
+      &:nth-last-child(3) {
+          display: none;
+     }}}
+  `;
+  
+  const LaunchContainer = styled.div`
+    position:relative;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 40px;
+  `;
+  
+  const ContainerTitle = styled.span`
+    position: absolute;
+    font-weight: bold;
+    left: calc(10% + 50px);
+    top: -4px;
+    padding: 0 12px;
+    font: Compita;
+    background-color: ${props => props.theme.bgColor};
+    font-size: ${props => props.theme.fontSizes.xs};
+    color: ${props => props.theme.color.gray};
+    display: flex;
+    flex-direction: row;
+    display: flex;
+    align-items: center;
+  `;
+  
+  const ContainerOutline = styled.div`
+    border-radius: 24px 24px 0 0;
+    border: 6px solid #383838;
+    border-bottom: none;
+    height: 40px;
+    width: 80%;
+    display: flex;
+    flex-direction: row;
+  `;
+  export default ArtistNfts;

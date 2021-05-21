1047e9164d269ef8deeb96f0e5319649f8954de2
pnkfluffy <jacksonfelty@gmail.com>
2021-05-21 13:01:18 -0500

    new contracts

--- src/web3/constants.js
+++ src/web3/constants.js
@@ -1,4 +1,4 @@
-export const NFTToken = "0xcbBf98aD5C7A06bDcf7CB2ddDFC6441d29e04b9E";
-export const Trade = "0xEacFf08eA27d3Bb377C9e06de62006C494DC4F51";
-export const FlatPriceSale = "0xfC9A40E3955C1747f49B2E5958cE7E7F3Ef47bbc";
-export const Auction = "0x73A1DcCBF0456579acE5F29cd13A13dC83c871bD";
+export const NFTToken = "0x038487d8D08B3eB349dC8B4a887467c98C147Ff0";
+export const Trade = "0x04b9E5215E3a825e44F3edc4EE158ff32b867fA5";
+export const FlatPriceSale = "0x15ab4435f9712B9784467adb3e57D5e2261796dd";
+export const Auction = "0x301485B95443ba84b1E499c3dE99EcB43daBBDD1";

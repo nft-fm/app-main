57c3783068f5a2cd255a81178e9f27ec2d458869
pnkfluffy <jacksonfelty@gmail.com>
2021-05-21 23:04:38 -0500

    dynamic chain dispatching

--- server/web3/constants.js
+++ server/web3/constants.js
@@ -1,10 +1,15 @@
-NFTToken = "0x56A1109767Fb39b048e2db760F6875FB3b3013e2";
-FlatPriceSale = "0x598cB39cB8049c5937b74f6570D97757F8EC8d7b";
-// Trade = "0x41DF6f9650B1EA66088116F6f6a603Ad4D7228bF";
-// Auction = "0xA27A1b7Fc88FbDB16D6DF862C4Cd8aB77bE3cA06";
+const TEST_NFTToken = "0x56A1109767Fb39b048e2db760F6875FB3b3013e2";
+const TEST_FlatPriceSale = "0x598cB39cB8049c5937b74f6570D97757F8EC8d7b";
+
+const MAIN_NFTToken = "";
+const MAIN_FlatPriceSale = "";
+
+const isMain = process.env.REACT_APP_IS_MAINNET;
+
+const NFTToken = isMain ? MAIN_NFTToken : TEST_NFTToken;
+const FlatPriceSale = isMain ? MAIN_FlatPriceSale : TEST_FlatPriceSale;
 
 module.exports = {
 NFTToken,
 FlatPriceSale,
-Auction,
 }
--- src/web3/constants.js
+++ src/web3/constants.js
@@ -1,4 +1,10 @@
-export const NFTToken = "0x56A1109767Fb39b048e2db760F6875FB3b3013e2";
-export const FlatPriceSale = "0x598cB39cB8049c5937b74f6570D97757F8EC8d7b";
-// export const Trade = "0x41DF6f9650B1EA66088116F6f6a603Ad4D7228bF";
-// export const Auction = "0xA27A1b7Fc88FbDB16D6DF862C4Cd8aB77bE3cA06";
+const TEST_NFTToken = "0x56A1109767Fb39b048e2db760F6875FB3b3013e2";
+const TEST_FlatPriceSale = "0x598cB39cB8049c5937b74f6570D97757F8EC8d7b";
+
+const MAIN_NFTToken = "";
+const MAIN_FlatPriceSale = "";
+
+const isMain = process.env.REACT_APP_IS_MAINNET;
+
+export const NFTToken = isMain ? MAIN_NFTToken : TEST_NFTToken;
+export const FlatPriceSale = isMain ? MAIN_FlatPriceSale : TEST_FlatPriceSale;

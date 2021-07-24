07cbbf4fb04885b84f4d752e01ce6d7b4ce1bfef
pnkfluffy <jacksonfelty@gmail.com>
2021-07-24 15:02:57 -0500

    status

--- server/web3/server-utils.js
+++ server/web3/server-utils.js
@@ -94,12 +94,6 @@ const getUserNfts = async (account) => {
   let walletWithProvider = new Wallet(process.env.OWNER_KEY, provider);
   const contract = new Contract(NFTToken, NFTTokenABI, walletWithProvider);
 
-  let userBalance = [];
-  const nftIds = [0, 1, 2, 3, 4, 5];
-  for (nftId of nftIds) {
-    const balance = await contract.balanceOf(account, nftId);
-    console.log("nft bal:", nftId, Number(balance));
-  }
   let userNfts = await contract.getFullBalance(account);
   // let nftIds = [];
   // let numNfts = [];

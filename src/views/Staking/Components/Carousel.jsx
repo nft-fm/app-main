9dca5e8eceff113ff2dab44a2edd7e4dbaa61e78
Quinn LaBrie <quinn.labrie@gmail.com>
2021-09-16 18:30:31 -0500

    carousel styling partially done

--- src/views/Staking/Components/Carousel.jsx
+++ src/views/Staking/Components/Carousel.jsx
@@ -4,34 +4,56 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 
 const CarouselHolder = ({ artists }) => {
+  const x = 100 / 3
   return (
     <Container>
       <h1>Carousel goes here</h1>
-      <Carousel
-        autoPlay
-        infiniteLoop
-        centerMode
-        centerSlidePercentage={33.3}
-      >
-        {artists.map((artist) => {
-          return (
-            <div>
-              <ProfilePic
-                src={artist.profilePic}
-                alt={`${artist.username}'s profile picture`}
-              />
-              <p className="legend">{artist.username}</p>
-            </div>
-          );
-        })}
-      </Carousel>
+      <Wrapper>
+        <Carousel
+          // autoPlay
+          infiniteLoop
+          centerMode
+          centerSlidePercentage={x}
+        >
+          {artists.map((artist) => {
+            return (
+              <ArtistHolder>
+                <ProfilePic
+                  src={artist.profilePic}
+                  alt={`${artist.username}'s profile picture`}
+                />
+                <Legend>{artist.username}</Legend>
+              </ArtistHolder>
+            );
+          })}
+        </Carousel>
+      </Wrapper>
     </Container>
   );
 };
 
+const Wrapper = styled.div`
+  border: 2px solid ${(props) => props.theme.color.boxBorder};
+`;
+
+const Legend = styled.p`
+  position: absolute;
+  bottom: 0px;
+  border: 2px solid ${(props) => props.theme.color.blue};
+  border-radius: 10px;
+  background: #000;
+  color: #fff;
+  padding: 10px;
+  font-size: ${(props) => props.theme.fontSizes.xs};
+  text-align: center;
+  transition: opacity 0.35s ease-in-out;
+`;
+
+const ArtistHolder = styled.div``;
 const ProfilePic = styled.img`
   width: 200px;
   height: 200px;
+  object-fit: contain;
 `;
 
 const Container = styled.div`
